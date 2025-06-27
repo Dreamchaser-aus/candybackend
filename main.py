681e416062de574648c806b1d71b4afdda9e6c2d
Dreamchaser-aus <dreamchaseraus8@gmail.com>
2025-06-27 21:10:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -101,6 +101,10 @@ def delete_user():
             conn.commit()
     return jsonify({"status": "ok"})
 
+@app.route("/")
+def index():
+    return redirect(url_for("admin"))
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
