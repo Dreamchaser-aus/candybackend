67e3496fc804d88f5382cec7e64433c5788ecf06
Dreamchaser-aus <dreamchaseraus8@gmail.com>
2025-07-07 18:19:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -431,6 +431,5 @@ def run_init_tables():
 
 if __name__ == "__main__":
     init_tables()   # 自动建表
-    fix_token_type()  # ★★ 这里加上，自动修正 token 字段为整数
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
